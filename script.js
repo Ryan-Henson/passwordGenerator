5114b0f83e96e38d59ce0f65d69ece81dca46fdb
Ryan-Henson <72321799+Ryan-Henson@users.noreply.github.com>
2020-11-02 07:57:32 -0600

    removed else if statments, redudeced to shorter ifs. worked with tutor to clean code up. moved vers from global to inside functions

--- script.js
+++ script.js
@@ -1,90 +1,60 @@
-let userImput;
-let userNumber;
-let userCharacter;
-let userUpperCase;
-let userLowerCase;
-let imput;
-
-let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
-let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-let character ="!@#$%^&*()".split("");
-
 let generateBtn = document.querySelector("#generate");
 
-generateBtn.addEventListener("click", writePassword)
-
-function writePassword() {
-    let password = generatePassword();
-    let passwordText = document.querySelector("#password");       
-    passwordText.value = password;           
-}
-
 function generatePassword() {
-    userImput = parseInt(prompt("Choose a number between 8 and 128 for your password length"));
-    if (!userImput) {
-        alert("Please pick a number between 8 and 128");
-    } else if (userImput < 8 || userImput > 128) {
-        userImput = parseInt(prompt("The number must be between 8 and 128"));
-    } else {
-        userNumber = confirm("Would you like numbers?");
-        userCharacter = confirm("Would you like special characters?");
-        userUpperCase = confirm("Would you like Uppercase letters?");
-        userLowerCase = confirm("Would you like Lowercase letters?");
-    };
-    if (!userCharacter && !userNumber && !userUpperCase && !userLowerCase) {
-        imput = alert("At least one criteria must be chosen");
-    }
-    else if (userCharacter && userNumber && userUpperCase && userLowerCase) {
-        imput = character.concat(number, lowerCase, upperCase);
-    }
-    else if (userCharacter && userNumber && userUpperCase) {
-        imput = character.concat(number, upperCase);
-    }
-    else if (userCharacter && userNumber && userLowerCase) {
-        imput = character.concat(number, lowerCase);
-    }
-    else if (userCharacter && userLowerCase && userUpperCase) {
-        imput = character.concat(lowerCase, upperCase);
-    }
-    else if (userNumber && userLowerCase && userUpperCase) {
-        imput = number.concat(lowerCase, upperCase);
-    }
-    else if (userCharacter && userNumber) {
-        imput = character.concat(number);
-    } 
-    else if (userCharacter && userLowerCase) {
-        imput = character.concat(lowerCase);
-    } 
-    else if (userCharacter && userUpperCase) {
-        imput = character.concat(upperCase);
-    }
-    else if (userLowerCase && userNumber) {
-        imput = lowerCase.concat(number);
-    } 
-    else if (userLowerCase && userUpperCase) {
-        imput = lowerCase.concat(upperCase);
-    } 
-    else if (userNumber && userUpperCase) {
-        imput = number.concat(upperCase);
-    }
-    else if (userCharacter) {
-        imput = character;
-    }
-    else if (confirmNumber) {
-        imput = number;
-    }
-    else if (userLowerCase) {
-        imput = lowerCase;
-    }
-    else if (userUpperCase) {
-        imput = upperCase;
-    };
-    let password = [];
-    for (let i = 0; i < userImput; i++) {
-        let pickInput = imput[Math.floor(Math.random() * imput.length)];
-        password.push(pickInput);
-    }
-    let outputInfo = password.join("");
-    return outputInfo;
+  let userImput;
+  let userNumber;
+  let userCharacter;
+  let userUpperCase;
+  let userLowerCase;
+  let imput = [];
+
+  let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  let character = "!@#$%^&*()".split("");
+  userImput = parseInt(
+    prompt("Choose a number between 8 and 128 for your password length")
+  );
+  if (!userImput) {
+    alert("Please pick a number between 8 and 128");
+    return "";
+  }
+  if (userImput < 8 || userImput > 128) {
+    alert("The number must be between 8 and 128");
+    return "";
+  }
+  userNumber = confirm("Would you like numbers?");
+  userCharacter = confirm("Would you like special characters?");
+  userUpperCase = confirm("Would you like Uppercase letters?");
+  userLowerCase = confirm("Would you like Lowercase letters?");
+
+  if (!userCharacter && !userNumber && !userUpperCase && !userLowerCase) {
+    alert("At least one criteria must be chosen");
+    return "";
+  }
+  if (userNumber) {
+    imput = imput.concat(number);
+  }
+  if (userCharacter) {
+    imput = imput.concat(character);
+  }
+  if (userUpperCase) {
+    imput = imput.concat(upperCase);
+  }
+  if (userLowerCase) {
+    imput = imput.concat(lowerCase);
+  }
+  let password = [];
+  for (let i = 0; i < userImput; i++) {
+    let pickInput = imput[Math.floor(Math.random() * imput.length)];
+    password.push(pickInput);
+  }
+  let outputInfo = password.join("");
+  return outputInfo;
 }
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+generateBtn.addEventListener("click", writePassword);
